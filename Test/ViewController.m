e06df0d99076013328d7d84843d3f2277ac2f513
kobe_dream <1228830203@qq.com>
2019-11-28 15:50:18 +0800

    提交一个测试demo

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -10,13 +10,35 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UILabel *titleLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self setupLayout];
+}
+
+- (void)setupLayout {
+    CGFloat width = [UIScreen mainScreen].bounds.size.width;
+    CGFloat height = [UIScreen mainScreen].bounds.size.height;
+    self.titleLabel.frame = CGRectMake(0, (height-100)*0.5, width, 100);
+}
+
+- (UILabel *)titleLabel {
+    if (!_titleLabel) {
+        _titleLabel = [[UILabel alloc] init];
+        _titleLabel.text = @"这是一个测试项目";
+        _titleLabel.textColor = [UIColor redColor];
+        _titleLabel.font = [UIFont systemFontOfSize:18];
+        _titleLabel.textAlignment = NSTextAlignmentCenter;
+        [self.view addSubview:_titleLabel];
+    }
+    return _titleLabel;
 }
 
 
